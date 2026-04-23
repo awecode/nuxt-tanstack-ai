4d1e240c77bd334aa0f35ee88d38ce5ff2c28322
Dipesh Acharya <dipesh@awecode.com>
2026-04-23 10:36:20 +0545

    chore: cleanup nuxt config vite settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,4 @@ export default defineNuxtConfig({
   modules: [
     '@comark/nuxt'
   ],
-  vite: {
-    plugins: [
-      {
-        name: 'fix-partial-json',
-        resolveId(id) {
-          if (id === 'partial-json') {
-            return this.resolve('partial-json/dist/index.js', undefined, { skipSelf: true })
-          }
-        },
-      },
-    ],
-    optimizeDeps: {
-      include: [
-        'partial-json',
-      ],
-    },
-    ssr: {
-      noExternal: ['partial-json'],
-    },
-  },
 })
